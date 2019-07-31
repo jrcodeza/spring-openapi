a3dc29888aa72850f35618b4d68d2d216d56abce
jremenec <remenec.jakub@gmail.com>
2019-07-31 22:10:39 +0200

    Prepare test case for javax.validation support

--- schema-generator/src/test/java/org/spring/openapi/schema/generator/model/Car.java
+++ schema-generator/src/test/java/org/spring/openapi/schema/generator/model/Car.java
@@ -1,9 +1,20 @@
 package org.spring.openapi.schema.generator.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
 public class Car extends Product {
 
+    @Min(0)
+    @Max(1000)
     private Integer torque;
+
+    @PositiveOrZero
     private Integer maxSpeed;
+
+    @Size(min = 2, max = 30)
     private String model;
 
     public Integer getTorque() {

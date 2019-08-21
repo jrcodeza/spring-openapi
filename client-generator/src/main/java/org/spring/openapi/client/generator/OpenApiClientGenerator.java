d6966799d00a1baeae06e3ac6b541436be681514
Jakub Remenec <jremenec@davincisoftware.sk>
2019-08-21 21:48:52 +0200

    Start implementing java generator

---
+++ client-generator/src/main/java/org/spring/openapi/client/generator/OpenApiClientGenerator.java
@@ -0,0 +1,119 @@
+package org.spring.openapi.client.generator;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.lang.model.element.Modifier;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.squareup.javapoet.AnnotationSpec;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeSpec;
+
+import io.swagger.v3.oas.integration.IntegrationObjectMapperFactory;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.ComposedSchema;
+import io.swagger.v3.oas.models.media.Discriminator;
+import io.swagger.v3.oas.models.media.Schema;
+
+public class OpenApiClientGenerator {
+
+	private List<SchemaEntry> classCreateOrder;
+
+	public void generateClient(String targetPackage, File openApiJson) {
+		try {
+			OpenAPI openAPI = IntegrationObjectMapperFactory.createJson().readValue(openApiJson, OpenAPI.class);
+			Map<String, Schema> schemas = openAPI.getComponents().getSchemas();
+			schemas.entrySet().forEach(schemaEntry -> processSchemaEntry(targetPackage, schemaEntry));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void processSchemaEntry(String targetPackage, Map.Entry<String, Schema> schemaEntry) {
+		Schema schema = schemaEntry.getValue();
+		TypeSpec.Builder typeSpecBuilder = TypeSpec.classBuilder(schemaEntry.getKey()).addModifiers(Modifier.PUBLIC);
+		if (schema instanceof ComposedSchema) {
+			ComposedSchema composedSchema = (ComposedSchema) schema;
+			typeSpecBuilder
+					.superclass(ClassName.get(targetPackage, getNameFromRef(composedSchema.getAllOf().get(0).get$ref()))) // TODO null check
+					.build();
+			schema = composedSchema.getAllOf().get(1);
+		}
+		if (schema.getDiscriminator() != null) {
+			parseDiscriminator(typeSpecBuilder, schema.getDiscriminator(), targetPackage);
+		}
+
+		try {
+			JavaFile.builder(targetPackage, typeSpecBuilder.build())
+					.build()
+					.writeTo(Paths.get("C:\\Users\\jremenec.DAVINCI\\Projects\\spring-openapi\\schema-generator\\target\\openapi"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void parseDiscriminator(TypeSpec.Builder typeSpecBuilder, Discriminator discriminator, String targetPackage) {
+		if (discriminator.getPropertyName() != null) {
+			AnnotationSpec jsonTypeInfoAnnotation = AnnotationSpec.builder(JsonTypeInfo.class)
+					.addMember("use", "$L", JsonTypeInfo.Id.NAME)
+					.addMember("include", "$L", JsonTypeInfo.As.PROPERTY)
+					.addMember("property", "$S", discriminator.getPropertyName())
+					.build();
+			typeSpecBuilder.addAnnotation(jsonTypeInfoAnnotation);
+		}
+		if (discriminator.getMapping() != null) {
+			List<AnnotationSpec> annotationSpecs = discriminator.getMapping().entrySet().stream()
+					.map(discriminatorMappingEntry ->
+							AnnotationSpec.builder(Type.class)
+									.addMember("value", "$T.class", ClassName.get(targetPackage, discriminatorMappingEntry.getValue()))
+									.addMember("name", "$S", discriminatorMappingEntry.getKey())
+									.build())
+					.collect(Collectors.toList());
+
+			AnnotationSpec jsonSubTypesAnnotation = AnnotationSpec.builder(JsonSubTypes.class)
+					.addMember("value", "$L", annotationSpecs)
+					.build();
+
+			typeSpecBuilder.addAnnotation(jsonSubTypesAnnotation);
+		}
+	}
+
+	private String getNameFromRef(String ref) {
+		return ref.replace("#/components/schemas/", "");
+	}
+
+	class SchemaEntry {
+		private String name;
+		private Schema schema;
+
+		public SchemaEntry(String name, Schema schema) {
+			this.name = name;
+			this.schema = schema;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Schema getSchema() {
+			return schema;
+		}
+
+		public void setSchema(Schema schema) {
+			this.schema = schema;
+		}
+	}
+
+}
